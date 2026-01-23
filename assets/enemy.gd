27bd89a61881128407b0ec1da789c21fc4314978
AlexStenshagen <alexstenshagen123@gmail.com>
2026-01-23 15:52:37 +0100

    IKKE FUNGERENDE

---
+++ assets/enemy.gd
@@ -0,0 +1,23 @@
+extends AnimatedSprite2D
+
+@onready var healthbar = $HealthBar
+
+var health = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	health = 100
+	healthbar.init_health(health)
+
+func take_damage(amount):
+	health = max(0, health - amount)
+	healthbar._set_health(health)
+	
+	if health <= 0:
+		queue_free() 
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	if area.is_in_group("bullet"):
+		take_damage(Global.player_damage)  
+		print("Enemy damaged")
+		
---
+++ global.gd
@@ -0,0 +1,10 @@
+extends Node
+
+# Global.gd
+var player_damage = 10
+var base_damage = 10
+var inventory = null
+
+#func _ready():
+	#inventory = preload("res://inventory/playerinv.tres").duplicate()
+	# Or create new: inventory = Inv.new()   
