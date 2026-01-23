9ac92b50c5c8f2f3c56cdc04dc57523903f7280b
AlexStenshagen <alexstenshagen123@gmail.com>
2026-01-23 14:47:03 +0100

    Map Beta Snnaaaart ferdig

--- scenes/alexplayer.gd
+++ scenes/alexplayer.gd
@@ -3,23 +3,47 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+const DASH_SPEED = 1200.0
 
+var dashing = false
+var can_dash = true
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+	
+	if Input.is_action_just_pressed("dash") and can_dash:
+		dashing = true
+		$dash_timer.start()
+		$dash_again_timer.start()
+		can_dash = false
+		
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+		
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("walk_left", "walk_right")
+	
 	if direction:
-		velocity.x = direction * SPEED
+		if dashing:
+			velocity.x = direction * DASH_SPEED
+		else:
+			velocity.x = direction * SPEED
+		
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
+
+
 	move_and_slide()
+
+
+func _on_dash_timer_timeout() -> void:
+	dashing = false 
+
+
+func _on_dash_again_timer_timeout() -> void:
+	can_dash = true
