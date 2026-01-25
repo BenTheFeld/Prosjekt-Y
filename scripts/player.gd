25c67954ee5a99c539ca95528feee20c4cd017be
BenTheFeld <benjamin.feldgendler@gmail.com>
2026-01-26 00:39:08 +0100

    deletes and renames player scene and script. wip walljumps

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,10 +3,9 @@ extends CharacterBody2D
 
 const SPEED = 200.0
 const JUMP_VELOCITY = -600.0
-const DASH_SPEED = 500.0 + SPEED
+const DASH_SPEED = 600.0 + SPEED
 const COYOTE_TIME = 0.3
 const MAX_JUMP_AMOUNT = 2
-const MAX_DASH_AMOUNT = 1
 
 var health = 0
 var last_direction = 1
@@ -17,7 +16,9 @@ var was_on_floor = true
 var was_on_wall
 var jumping = false
 var jump_amount = MAX_JUMP_AMOUNT
-var dash_amount = MAX_DASH_AMOUNT
+var dash_ready = true
+var walljump_ready = false
+var walljumping = false
 @onready var healthbar = $CanvasLayer/HealthBar
 @onready var damage_timer = $damage_timer
 @onready var coyote_timer: Timer = $coyote_timer
@@ -48,22 +49,21 @@ func _physics_process(delta: float) -> void:
 		hands_sprite.flip_h = false
 	
 	#moves player
-	if direction and !dashing:
+	if direction and !dashing and !walljumping:
 		velocity.x = direction * SPEED
 	else: 
 		velocity.x = move_toward(velocity.x, 0, 40)
 
 	#triggers dashes
-	if Input.is_action_just_pressed("dash") and can_dash and dash_amount > 0:
+	if Input.is_action_just_pressed("dash") and can_dash and dash_ready:
 		#velocity.x = last_direction * DASH_SPEED
-		dash_amount -= 1
+		dash_ready = false
 		velocity = mouse_direction * DASH_SPEED
 		dashing = true
 		can_dash = false
 		$dash_duration_timer.start()
 		$dash_cooldown_timer.start()
 		
-	
 	# Handle jump.
 	
 	#saves if the player was on the floor or not in the last frame
@@ -76,26 +76,33 @@ func _physics_process(delta: float) -> void:
 	#resets jump status after landing in the floor
 	if is_on_floor():
 		jumping = false
+		walljump_ready = false
+		walljumping = false
 		coyote_timer.stop()
 		jump_amount = MAX_JUMP_AMOUNT
-		dash_amount = MAX_DASH_AMOUNT
+		dash_ready = true
 	
 	#slows the players decent when colliding with a wall
 	if is_on_wall() and last_direction == direction and !jumping:
 		velocity = get_gravity() * 0.5
 		hands_sprite.visible = true
-		if Input.is_action_just_pressed("dash"):
-			velocity = mouse_direction * SPEED
-			print(mouse_direction * SPEED)
-			print("mouse dash")
+		#walljumps
+		if Input.is_action_just_pressed("jump") and walljump_ready:
+			velocity = mouse_direction * DASH_SPEED
+			jumping = true
+			jump_amount -= 1
+			coyote = false
+			walljump_ready = false
+			walljumping = true
 	else:
 		hands_sprite.visible = false
 		velocity += get_gravity()
 	
 	#when the player touches a wall they gain a jump
 	if is_on_wall_only() and !was_on_wall:
-		jump_amount = 1
+		walljump_ready = true
 		jumping = false
+		walljumping = false
 		
 	#jump action
 	if Input.is_action_just_pressed("jump") and (jump_amount > 0 or is_on_floor() or coyote):
