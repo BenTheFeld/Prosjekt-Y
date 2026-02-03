f814dc9790e850e6b8882930a9e7afd7e2696a08
AlexStenshagen <alexstenshagen123@gmail.com>
2026-02-03 13:03:02 +0100

    Fungerende HS men stygg

--- scripts/alexplayer.gd
+++ scripts/alexplayer.gd
@@ -1,6 +1,5 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 const DASH_SPEED = 1200.0
@@ -9,7 +8,7 @@ var can_take_damage: bool = true
 
 var dashing = false
 var can_dash = true
-@onready var healthbar = $CanvasLayer/HealthBar
+@onready var healthbar = $CanvasLayer/Node2D/HealthBar
 @onready var timer = $Timer 
 @onready var screenShake = get_parent()
 
@@ -17,69 +16,57 @@ func _ready():
 	health = 5
 	healthbar.init_health(health)
 
-
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	# Apply gravity
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	
+	# Handle dash
 	if Input.is_action_just_pressed("dash") and can_dash:
 		dashing = true
 		$dash_timer.start()
 		$dash_again_timer.start()
 		can_dash = false
 		
-	# Handle jump.
+	# Handle jump
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Horizontal movement
 	var direction := Input.get_axis("walk_left", "walk_right")
-	
 	if direction:
-		if dashing:
-			velocity.x = direction * DASH_SPEED
-		else:
-			velocity.x = direction * SPEED
-		
+		velocity.x = direction * (DASH_SPEED if dashing else SPEED)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
-
-
 	move_and_slide()
 
+# Take damage and start invincibility
 func take_damage(amount):
 	health = max(0, health - amount)
 	healthbar._set_health(health)
 	if health <= 0:
 		get_tree().change_scene_to_file("res://scenes/map.tscn")
 
+# Check for overlapping enemies every frame
+func _process(delta):
+	if !can_take_damage:
+		return
+	var overlapping_areas = $Area2D.get_overlapping_areas()
+	for area in overlapping_areas:
+		if area.is_in_group("enemy"):
+			screenShake.apply_noise_shake()
+			take_damage(1)
+			can_take_damage = false
+			timer.start()
+			break
+
 func _on_dash_timer_timeout() -> void:
 	dashing = false 
-
 
 func _on_dash_again_timer_timeout() -> void:
 	can_dash = true
 
-
-#DETTE SKAL LIMES INN 
-func _on_area_2d_area_entered(area: Area2D) -> void:
-	if !can_take_damage:
-		return
-	if area.is_in_group("enemy"):
-		screenShake.apply_noise_shake()
-		take_damage(1)
-		Global.healthvis = true
-		can_take_damage = false
-		timer.start()
-
+# Invincibility ends, allow damage again
 func _on_timer_timeout() -> void:
-	can_take_damage = true
-	take_damage(1)
-	screenShake.apply_noise_shake()
-
-
-func _on_area_2d_area_exited(area: Area2D) -> void:
-	pass
+	can_take_damage = true   
